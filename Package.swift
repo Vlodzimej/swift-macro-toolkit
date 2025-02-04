85c436f744534627b90ed58faf70bf4641c3d8e0
Амелькин Владимир <amelkin@vbankcenter.ru>
2025-02-04 14:17:21 +0300

    Changed macOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-macro-toolkit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v10_13), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         .library(
             name: "MacroToolkit",
